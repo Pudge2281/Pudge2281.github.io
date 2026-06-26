dacd298d48dc00312463800bea1af8f554df5402
Pudge2281 <86096687+Pudge2281@users.noreply.github.com>
2026-06-26 18:21:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("BLACKPINK AI GAME");
+document.querySelectorAll('.video-thumbnail').forEach(video => {
+    video.addEventListener('click', () => {
+        const videoId = video.getAttribute('data-video');
+        alert(`Здесь запустится плеер для ВИДЕО ${videoId}`);
+    });
+});
+
+document.querySelector('.play-now-btn').addEventListener('click', () => {
+    alert('Запуск игры!');
+});
